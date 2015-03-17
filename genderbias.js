4718732c12d4f38a875f1c2a77fcfaa7c918ab6d
Stuart <gravatar@strudel.org.uk>
2015-03-17 16:32:41 +0000

    Add some example letters

--- genderbias.js
+++ genderbias.js
@@ -23,7 +23,68 @@ function textChanged() {
 	}
 }
 
+var examples = [];
 function example() {
-	document.getElementById("recommendationLetter").value = "Melinda was one of the first users of my now widely-used and successful software, MetNetMaker. Her early bug reports and insightful suggestions were invaluable to making the product what it is today. I have not since worked with anyone so at ease communicating with those in other scientific fields."
+	var v = "Melinda was one of the first users of my now widely-used and successful software, MetNetMaker. Her early bug reports and insightful suggestions were invaluable to making the product what it is today. I have not since worked with anyone so at ease communicating with those in other scientific fields.";
+	if(examples.length > 0){
+		var i = Math.floor(Math.random()*examples.length);
+		v = parseMarkdown(examples[i]);
+	}
+	document.getElementById("recommendationLetter").value = v;
 	textChanged();
+}
+
+function parseMarkdown(data){
+	if(typeof data==="string") data = data.split(/[\n\r]/);
+	// If it looks like YAML we remove it
+	start = 0;
+	if(data[0].indexOf('---')==0){
+		for(var i = 1; i < data.length; i++){
+			if(data[i].indexOf('---')==0){
+				start = i+1;
+				if(data[i+1]=="") start++;
+				continue;
+			}
+		}
+	}
+	var out = "";
+	for(var i = start; i < data.length; i++){
+		out += data[i]+'\n';
+	}
+	return out;
+}
+
+function getExamples(data,attr){
+	if(!attr) var attr = { i: 1 };
+	if(data){ examples.push(data); }
+	attr.i++;
+	//attr.error = loadedExamples;
+	loadFILE('examples/'+(attr.i-1)+'.md',getExamples,attr);
+}
+getExamples();
+
+// Function to load a file (same domain)
+function loadFILE(file,fn,attrs){
+	if(!attrs) attrs = {};
+	attrs['_file'] = file;
+	var error = "";
+	var xhr = new XMLHttpRequest();
+	if(attrs.error && typeof attrs.error==="function") error = function(e){ attrs.error.call((attrs.context ? attrs.context : this),e,attrs) }
+	if(error){
+		xhr.addEventListener("error", error, false);
+		xhr.addEventListener("abort", error, false);
+	}
+	xhr.onreadystatechange = function(){
+		if(xhr.readyState==4){
+			if(typeof fn==="function"){
+				fn.call((attrs.context ? attrs.context : this),xhr.responseText,attrs);
+			}
+		}
+	}
+	xhr.open("GET", file, true);
+	try {
+		xhr.send();
+	} catch(e) {
+		if(error) attrs.error.call((attrs.context ? attrs.context : this),e,attrs);
+	}
 }
